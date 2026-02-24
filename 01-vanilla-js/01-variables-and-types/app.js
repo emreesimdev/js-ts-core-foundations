0798d1d8171419c5fc1df64074084eff06f0e164
emreesimdev <emreesim.dev@gmail.com>
2026-02-24 15:25:21 +0300

    feat: add core data types and variable declarations:

---
+++ 01-vanilla-js/01-variables-and-types/app.js
@@ -0,0 +1,13 @@
+// --- SECTION 1: THE AWAKENING (HELLO WORLD) ---
+
+console.log("Hello World!");
+
+// --- SECTION 2: VARIABLES & DATA TYPES ---
+
+const myName = "Emre";
+let age = 24;
+const studentStatus = true;
+
+console.log(
+  `Hi, I am ${myName}, I am ${age} years old and my student status is: ${studentStatus}`,
+);
