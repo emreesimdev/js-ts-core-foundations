150b8fbc602dc6763a2e34e517de4d673f99e1b5
emreesimdev <emreesim.dev@gmail.com>
2026-02-25 17:07:15 +0300

    feat: implement control flow for user eligibility validation

--- 01-vanilla-js/01-variables-and-types/app.js
+++ 01-vanilla-js/01-variables-and-types/app.js
@@ -11,3 +11,13 @@ const studentStatus = true;
 console.log(
   `Hi, I am ${myName}, I am ${age} years old and my student status is: ${studentStatus}`,
 );
+
+// --- SECTION 3: CONTROL FLOW (IF/ELSE) ---
+
+if (age >= 18 && studentStatus === true) {
+  console.log(
+    "Access granted: Eligible for global remote work and student discount",
+  );
+} else {
+  console.log("Standard procedure applied");
+}
