50d4fdb6526197e2bc75d21fdf932ed4fb499ff7
emreesimdev <emreesim.dev@gmail.com>
2026-03-03 17:56:35 +0300

    feat: setup data structures directory and implement while loop for sensor simulation

---
+++ 01-vanilla-js/02-data-structures/app.js
@@ -0,0 +1,9 @@
+// --- SECTION 1: ARRAYS & COLLECTIONS (INTRODUCTION) ---
+// Note: While loops are often used for unknown iteration counts, like sensor data.
+
+let temperature = -15;
+
+while (temperature >= -18) {
+  console.log(`ALARM: Temperature is ${temperature}! Cooling down...`);
+  temperature--;
+}
