de29e722d75325857f25553dea140d8a65d24b0d
emreesimdev <emreesim.dev@gmail.com>
2026-03-08 23:56:39 +0300

    feat: mutate cargo object by updating, adding and deleting properties

--- 01-vanilla-js/03-objects/app.js
+++ 01-vanilla-js/03-objects/app.js
@@ -14,3 +14,13 @@ console.log(`Current cargo weight: ${cargoProfile.weight}`);
 
 let dynamicKey = "id";
 console.log(`Dynamic query result: ${cargoProfile[dynamicKey]}`);
+
+// --- SECTION 3: OBJECT MUTATION (ADD, UPDATE, DELETE) ---
+
+cargoProfile.weight = 15.5; // Update
+
+cargoProfile.customsTax = 45; // Add
+
+delete cargoProfile.isDelivered; // Delete
+
+console.log(cargoProfile);
