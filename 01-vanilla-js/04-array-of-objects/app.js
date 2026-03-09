13c67f12185fd724ac012579d268e0aebf8a336b
emreesimdev <emreesim.dev@gmail.com>
2026-03-09 14:56:08 +0300

    feat: implement array of objects and dynamic property access

---
+++ 01-vanilla-js/04-array-of-objects/app.js
@@ -0,0 +1,9 @@
+// --- SECTION 1: ARRAY OF OBJECTS (MANIFEST) ---
+
+const cargoManifest = [
+  { id: "TR-001", weight: 10, isActive: true },
+  { id: "DE-002", weight: 25, isActive: true },
+  { id: "NL-003", weight: 15, isActive: false },
+];
+
+console.log(cargoManifest[1].weight);
