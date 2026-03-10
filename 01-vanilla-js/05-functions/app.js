0b0ba9b0141dda19112ab4e27b10546fd613253c
emreesimdev <emreesim.dev@gmail.com>
2026-03-10 16:56:41 +0300

    feat: implement modern arrow functions with explicit and implicit returns

--- 01-vanilla-js/05-functions/app.js
+++ 01-vanilla-js/05-functions/app.js
@@ -10,3 +10,17 @@ function calculateCustomsTax(weight) {
 
 console.log(calculateCustomsTax(25));
 console.log(calculateCustomsTax(10));
+
+// --- SECTION 2: ARROW FUNCTIONS (MODERN SYNTAX) ---
+
+const calculateTotalFreight = (weight, distance) => {
+  return weight * 10 + distance * 2;
+};
+
+console.log(calculateTotalFreight(20, 150));
+
+// --- SECTION 3: ARROW FUNCTIONS (IMPLICIT RETURN) ---
+
+const calculateFastFreight = (weight, distance) => weight * 10 + distance * 2;
+
+console.log(calculateFastFreight(30, 170));
